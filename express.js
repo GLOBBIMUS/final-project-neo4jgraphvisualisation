58dab93692cd28a0589305febee2a2dfdedfdd84
GLOBBIMUS <striker1342566@gmail.com>
2017-12-11 23:17:13 -0600

    make API getWinners functional, change json output from neo4J query

--- express.js
+++ express.js
@@ -8,29 +8,39 @@ const neo4j = require('neo4j-driver').v1;
 const driver = neo4j.driver("bolt://127.0.0.1:7687", neo4j.auth.basic("neo4j", "password"));
 const session = driver.session();
 
-app.get("/totalError", function(req, res) {
-  var totalError = req.param('totalError');
+
+app.get("/getWinners",function(req,res){ // handles the /getWinners API. Gets Individuals whose TotalError is 0
   const actorPromise = session.run(
-    "MATCH(n: Individual) - [HasTotalError] -> (x: TotalError {TotalError: " + totalError + "}) RETURN n LIMIT 100;"
-  )
-  .then(function(result) {
+    "MATCH(n: Individual) - [HasTotalError] -> (x: TotalError {TotalError:  0 }) RETURN n LIMIT 100;"
+  ).then(function(result) {
     var fields = packageFields(result);
     res.send(fields);
-    console.log("Node: ");
-    console.log(node);
     session.close();
+    driver.close();
   });
 });
+
 
 function packageFields(result) {
   var fields = [];
   for(i = 0; i < result.records.length; i++) {
-    var individual = result.records[i]._fields;
-    //console.log(individual);
-    fields[i] = individual;
+    var individual = result.records[i]._fields[0];
+    var transformedIndividual = transform(individual);
+    console.log(transformedIndividual);
+    fields[i] = transformedIndividual;
   }
   console.log(fields);
   return fields;
+}
+
+function transform(individual){
+  var transformedIndividual = {};
+  transformedIndividual.identity = individual.identity.low;
+  transformedIndividual.label = individual.labels[0];
+  transformedIndividual.generation = individual.properties.generation.low;
+  transformedIndividual.location = individual.properties.location.low;
+  transformedIndividual.uuid = individual.properties.uuid;
+  return transformedIndividual;
 }
 
 app.listen(port);
